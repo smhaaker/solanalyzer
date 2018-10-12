ffdb105adc5b0b2e40b49c8301424e601f849b4c
Steffen Haaker <steffen.haaker@gmail.com>
2018-10-12 08:45:52 -0400

    Minor fixes

--- src/components/TextReader.vue
+++ src/components/TextReader.vue
@@ -24,8 +24,6 @@
     </div>
     <p></p>
   <p>{{text}}</p>
-
-
   <!-- <p>{{list}}</p> -->
   </div>
 </template>
@@ -41,7 +39,7 @@ export default {
       contractArray: null,
       functionList: [],
       functionsLoaded: null,
-      list: [], 
+      list: [],
       ok: null
     }
   },
@@ -82,43 +80,43 @@ export default {
     analyze () {
       console.log('analyze this!')
       let vm = this
-    //  this.findContractName()
-    //  this.findFunctions()
+      // this.findContractName()
+      // this.findFunctions()
       this.findElements(vm.text, 'function ', '{', vm.functionList)
       this.findElements(vm.text, 'contract ', '{', vm.list) // check this one
     },
-    findContractName () {
-      let vm = this
-      let preString = 'contract'
-      let searchString = '{'
-      let preIndex = vm.text.indexOf(preString)
-      let postIndex = vm.text.indexOf(searchString)
-      let searchIndex = preIndex + vm.text.substring(preIndex).indexOf(searchString)
-      console.log(preIndex)
-      console.log(postIndex)
-      vm.contractName = vm.text.slice(preIndex + 9, searchIndex)
-      console.log(vm.text.slice(preIndex + 9, searchIndex))
-    },
-    findFunctions () {
-      let vm = this
-      let preString = 'function '
-      let searchString = '{'
-      let preIndex = vm.text.indexOf(preString)
-      let postIndex = vm.text.indexOf(searchString)
-      let searchIndex = preIndex + vm.text.substring(preIndex).indexOf(searchString)
-      console.log(preIndex)
-      console.log(postIndex)
-      console.log(searchIndex)
-      let result = []
-      vm.functionList = vm.text.slice(preIndex + 9, searchIndex)
-      console.log('function? ' + vm.text.slice(preIndex + 9, searchIndex))
-      console.log(result)
-    },
+    // findContractName () {
+    //   let vm = this
+    //   let preString = 'contract'
+    //   let searchString = '{'
+    //   let preIndex = vm.text.indexOf(preString)
+    //   let postIndex = vm.text.indexOf(searchString)
+    //   let searchIndex = preIndex + vm.text.substring(preIndex).indexOf(searchString)
+    //   console.log(preIndex)
+    //   console.log(postIndex)
+    //   vm.contractName = vm.text.slice(preIndex + 9, searchIndex)
+    //   console.log(vm.text.slice(preIndex + 9, searchIndex))
+    // },
+    // findFunctions () {
+    //   let vm = this
+    //   let preString = 'function '
+    //   let searchString = '{'
+    //   let preIndex = vm.text.indexOf(preString)
+    //   let postIndex = vm.text.indexOf(searchString)
+    //   let searchIndex = preIndex + vm.text.substring(preIndex).indexOf(searchString)
+    //   console.log(preIndex)
+    //   console.log(postIndex)
+    //   console.log(searchIndex)
+    //   let result = []
+    //   vm.functionList = vm.text.slice(preIndex + 9, searchIndex)
+    //   console.log('function? ' + vm.text.slice(preIndex + 9, searchIndex))
+    //   console.log(result)
+    // },
     findElements (source, find, endChar, listNew) {
       let vm = this
       let startString = []
       let result = []
-      let totalIndex
+      // let totalIndex
       for (let i = 0; i < source.length; ++i) {
         // If you want to search case insensitive use
         // if (source.substring(i, i + find.length).toLowerCase() == find) {
@@ -126,25 +124,27 @@ export default {
           startString.push(i)
         }
       }
-      if(source.length > 0){
-        this.functionsLoaded = true;
+      if (source.length > 0) {
+        this.functionsLoaded = true
       }
       console.log(startString)
       // console.log(vm.text.slice(startString[0] + 9, startString[0] + 20)) // just a test
+      // Loop to find start and end of new string.
       for (let i = 0; i < startString.length; i++) {
         let searchIndex = startString[i] + vm.text.substring(startString[i]).indexOf(endChar)
-       // console.log('Search Indexes ' + searchIndex)
+        // console.log('Search Indexes ' + searchIndex)
         console.log(vm.text.slice(startString[i] + 9, searchIndex))
         // result.push(vm.text.slice(startString[i] + 9, searchIndex))
+        // pushes found word to result array
         let funct = vm.text.slice(startString[i] + 9, searchIndex)
         result.push(funct)
       // console.log(vm.text.slice(startString[i] + 9, startString[i] + 40))
-        // need to loop over startStrings here and find end of string as well.
       }
       console.log('result: ' + result)
       // vm.result = vm.result.concat(result)
       // this.result.push(...result)
-      for( let i = 0; i < result.length; i++){
+      // Loop over result array to push to data object.
+      for (let i = 0; i < result.length; i++) {
         console.log('arrayLoop: index: ' + i + ' is: ' + result[i])
         listNew.push({id: i, content: result[i]})
         // this.list.push({id: i})
