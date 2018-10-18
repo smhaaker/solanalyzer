4d3403ba430b1479c28b4444581cee8349bfe47c
Steffen Haaker <steffen.haaker@gmail.com>
2018-10-18 10:12:48 -0400

    Cleaned up and styled (poorly)

--- src/components/TextReader.vue
+++ src/components/TextReader.vue
@@ -1,33 +1,41 @@
 <template>
   <div>
+    <h2>Solidity Contract Analyzer</h2>
+    <p>Please load a smart contract and analyze, for now it only outputs functions and contract name.</p>
+
     <label class="text-reader">
       Read File
       <input type="file" @change="loadTextFromFile" multiple>
     </label>
-  <p>Files:</p>
-  <ul id="fileRender">
+
+  <button class="text-reader" @click="analyze">analyze</button>
+
+  <div id="filesList">
+  <ul id="fileRender"><h2 v-show="filesLoaded">Files Loaded:</h2>
     <li v-for="file in files" :key="file.index">
       Name: {{ file.name }}
       Size: {{ file.size }}
       Last Modified: {{ file.lastModifiedDate}}
     </li>
   </ul>
-  <button class="button" @click="analyze">analyze</button>
-  <div v-for="li in list" :key="li.index">
-    Contract Name: {{li.content}}
-        <div class="functionsList" v-for="funct in functionList" :key="funct.index">
-    {{funct.content}}
   </div>
-  <p></p>
+  <div class="functions">
+    <div v-for="li in list" :key="li.index">
+      <h4 class="contractTitle">Contract Name: {{li.content}}</h4>
+      <h4 class="title" v-show="functionsLoaded">Functions:</h4>
+      <div class="functionsList" v-for="funct in functionList" :key="funct.index">
+      {{funct.id}} {{funct.content}}
+      </div>
+    <p></p>
     </div>
+  </div>
   <!-- <p>Contract Name: {{contractName}}</p> -->
-  <h1 v-show="functionsLoaded">Functions:</h1>
   <!-- <div v-if="functionList > 0">test</div> -->
     <!-- <div class="functionsList" v-for="funct in functionList" :key="funct.index">
       {{funct.content}}
     </div>
     <p></p> -->
-  <p>{{text}}</p>
+  <!-- <p>{{text}}</p> -->
   <!-- <p>{{list}}</p> -->
   </div>
 </template>
@@ -43,6 +51,7 @@ export default {
       contractArray: null,
       functionList: [],
       functionsLoaded: null,
+      filesLoaded: null,
       list: [],
       ok: null
     }
@@ -50,30 +59,31 @@ export default {
   methods: {
     loadTextFromFile (ev) {
       var vm = this
+      vm.filesLoaded = true;
       console.log(ev.target.files)
       console.log(this.files)
       this.files = ev.target.files
       console.log('this files:' + this.files)
 
       const file = ev.target.files[0]
-      const textType = /text.*/
+      // const textType = /text.*/
       // if (file.type.match(textType)) {
-        var reader = new FileReader()
+      var reader = new FileReader()
 
-        reader.onload = function (e) {
-          // console.log(reader.result);
-          vm.text = reader.result
-          console.log('text result: ' + this.text)
-          let n = reader.result.search('contract')
-          console.log('contract name found at: ' + n)
-          vm.contractName = reader.result.slice(n, n + 20)
-          // console.log(reader.result.slice(n, n+20))
-          console.log(vm.contractName)
-        }
-        reader.readAsText(file)
-        // this.text = reader.result;
+      reader.onload = function (e) {
+        // console.log(reader.result);
+        vm.text = reader.result
+        console.log('text result: ' + this.text)
+        let n = reader.result.search('contract')
+        console.log('contract name found at: ' + n)
+        vm.contractName = reader.result.slice(n, n + 20)
+        // console.log(reader.result.slice(n, n+20))
+        console.log(vm.contractName)
+      }
+      reader.readAsText(file)
+      // this.text = reader.result;
       // } else {
-        // console.log('File not supported!')
+      // console.log('File not supported!')
       // }
       // const reader = new FileReader()
       // reader.onload = e => this.$emit('load', e.target.result)
@@ -84,43 +94,13 @@ export default {
     analyze () {
       console.log('analyze this!')
       let vm = this
-      // this.findContractName()
-      // this.findFunctions()
       this.findElements(vm.text, 'function ', '{', vm.functionList)
       this.findElements(vm.text, 'contract ', '{', vm.list) // check this one
     },
-    // findContractName () {
-    //   let vm = this
-    //   let preString = 'contract'
-    //   let searchString = '{'
-    //   let preIndex = vm.text.indexOf(preString)
-    //   let postIndex = vm.text.indexOf(searchString)
-    //   let searchIndex = preIndex + vm.text.substring(preIndex).indexOf(searchString)
-    //   console.log(preIndex)
-    //   console.log(postIndex)
-    //   vm.contractName = vm.text.slice(preIndex + 9, searchIndex)
-    //   console.log(vm.text.slice(preIndex + 9, searchIndex))
-    // },
-    // findFunctions () {
-    //   let vm = this
-    //   let preString = 'function '
-    //   let searchString = '{'
-    //   let preIndex = vm.text.indexOf(preString)
-    //   let postIndex = vm.text.indexOf(searchString)
-    //   let searchIndex = preIndex + vm.text.substring(preIndex).indexOf(searchString)
-    //   console.log(preIndex)
-    //   console.log(postIndex)
-    //   console.log(searchIndex)
-    //   let result = []
-    //   vm.functionList = vm.text.slice(preIndex + 9, searchIndex)
-    //   console.log('function? ' + vm.text.slice(preIndex + 9, searchIndex))
-    //   console.log(result)
-    // },
     findElements (source, find, endChar, listNew) {
       let vm = this
       let startString = []
       let result = []
-      // let totalIndex
       for (let i = 0; i < source.length; ++i) {
         // If you want to search case insensitive use
         // if (source.substring(i, i + find.length).toLowerCase() == find) {
@@ -128,11 +108,11 @@ export default {
           startString.push(i)
         }
       }
+      // display text on webpage if functions are loaded. Clear this to any.
       if (source.length > 0) {
         this.functionsLoaded = true
       }
       console.log(startString)
-      // console.log(vm.text.slice(startString[0] + 9, startString[0] + 20)) // just a test
       // Loop to find start and end of new string.
       for (let i = 0; i < startString.length; i++) {
         let searchIndex = startString[i] + vm.text.substring(startString[i]).indexOf(endChar)
@@ -145,13 +125,12 @@ export default {
       // console.log(vm.text.slice(startString[i] + 9, startString[i] + 40))
       }
       console.log('result: ' + result)
-      // vm.result = vm.result.concat(result)
       // this.result.push(...result)
-      // Loop over result array to push to data object.
+      // Loop over result array to push to data object
       for (let i = 0; i < result.length; i++) {
         console.log('arrayLoop: index: ' + i + ' is: ' + result[i])
+        listNew.splice(i) // empty old array
         listNew.push({id: i, content: result[i]})
-        // this.list.push({id: i})
       }
       console.log(listNew)
       // return startString
@@ -179,11 +158,35 @@ export default {
   z-index: -1;
   opacity: 0;
 }
+.functions{
+  align-content: left;
+  background-color: darkgray;
+  border-top-left-radius: 15px;
+}
 .functionsList{
   background-color: gray;
   text-align: left;
   padding: 5px;
   border-radius: 5px;
   margin-top: 5px;
+  width: 30%;
+}
+.title{
+  text-align: left;
+  padding: 5px;
+  background-color: darkgreen;
+  width: 30%;
+}
+.contractTitle{
+  text-align: left;
+  padding: 5px;
+  background-color: aqua;
+  border-top-left-radius: 15px;
+}
+#fileRender{
+  list-style: none;
+}
+#filesList{
+  text-align: left;
 }
 </style>
